346f0cabaa8029da272d0064312c82c357316778
Bram van Andel <andelvanbram@gmail.com>
2024-10-23 15:51:25 +0200

    Refactor import paths in minecraft.js and index.html

--- minecraft/js/minecraft.js
+++ minecraft/js/minecraft.js
@@ -1,8 +1,10 @@
-import { loadAndInitializeModel } from './render.js';
+import { loadAndInitializeModel } from '../js/render.js';
   
   document.addEventListener('DOMContentLoaded', () => {
+    // Specify the model path and container ID
     const modelPath = '../models/steve/';
     const containerId = 'steveModel';
     
+    // Call the function to load and initialize the model
     loadAndInitializeModel(modelPath, containerId);
   });
